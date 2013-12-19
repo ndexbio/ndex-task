2190c8867246c25a5ed445a5463a71fe3535c074
fcriscuo <fcriscu1@jhu.edu>
2013-12-18 17:22:01 -0800

    support for asynchronous file upload

--- src/main/java/org/ndexbio/task/FileUploadTask.java
+++ src/main/java/org/ndexbio/task/FileUploadTask.java
@@ -24,22 +24,22 @@ import org.slf4j.LoggerFactory;
 public class FileUploadTask extends NdexTask {
 
 	private final String filename;
-	private static final Logger logger = LoggerFactory.getLogger(FileUploadTask.class);
-	
+	private static final Logger logger = LoggerFactory
+			.getLogger(FileUploadTask.class);
+
 	private Status taskStatus;
 
 	public FileUploadTask(String taskId) throws IllegalArgumentException,
 			SecurityException, NdexException {
 		super(taskId);
-		this.filename = Configuration.getInstance().getProperty(
-				Configuration.UPLOADED_NETWORKS_PATH_PROPERTY)
-				+ this.getTask().getResource();
+		 this.filename = Configuration.getInstance().getProperty(
+		Configuration.UPLOADED_NETWORKS_PATH_PROPERTY)
+		 + this.getTask().getResource();
+		//this.filename = this.getTask().getResource();
 		if (!(new File(this.filename).isFile())) {
 			throw new NdexException("File " + this.filename + " does not exist");
 		}
 	}
-
-	
 
 	@Override
 	public Task call() throws Exception {
@@ -47,60 +47,62 @@ public class FileUploadTask extends NdexTask {
 		return this.getTask();
 	}
 
-	
 	protected String getFilename() {
 		return this.filename;
-		
-	}
-	
-	private void processFile ()  {
-		logger.info("Processing file: " +this.getFilename());
-		this.taskStatus = Status.PROCESSING;
-		File file = new File(this.getFilename());
-		switch(com.google.common.io.Files.getFileExtension(this.getFilename()) ){
-	      case("sif"):
-	    	  try {
-				final SIFFileParser sifParser = new SIFFileParser(file.getAbsolutePath());
-				  sifParser.parseSIFFile();
-				  this.taskStatus = Status.COMPLETED;
-			} catch (Exception e) {
-				this.taskStatus =Status.COMPLETED_WITH_ERRORS;
-				logger.error(e.getMessage());
-			}
-	      break;
-	      case("xbel"):
-	    	  try {
-				final XbelFileParser xbelParser = new XbelFileParser(file.getAbsolutePath());
-					if (!xbelParser.getValidationState().isValid()){
-						this.taskStatus =Status.COMPLETED_WITH_ERRORS;
-						throw new NdexException("XBEL file is has invalid elements.");
-					}	
-					xbelParser.parseXbelFile();
-					 this.taskStatus = Status.COMPLETED;
-			} catch (Exception e) {
-				this.taskStatus =Status.COMPLETED_WITH_ERRORS;
-				logger.error(e.getMessage());
-			}
-	      break;
-	      case("xlsx"):
-	      case("xls"):
-	    	  try {
-				final ExcelFileParser excelParser = new ExcelFileParser(file.getAbsolutePath());
-				  excelParser.parseExcelFile();
-				  this.taskStatus = Status.COMPLETED;
-			} catch (Exception e) {
-				this.taskStatus =Status.COMPLETED_WITH_ERRORS;
-				logger.error(e.getMessage());
-			}
-	      default:
-	         file.delete();
-	        logger.error
-	         	("The uploaded file type is not supported; must be SIF, XBEL, or XLSX.");
-	        this.taskStatus =Status.COMPLETED_WITH_ERRORS;
-	         
-	}
+
 	}
 
-	
+	private void processFile() throws Exception {
+		logger.info("Processing file: " + this.getFilename());
+		this.taskStatus = Status.PROCESSING;
+		this.startTask();
+		File file = new File(this.getFilename());
+		switch (com.google.common.io.Files.getFileExtension(this.getFilename())) {
+		case ("sif"):
+			try {
+				final SIFFileParser sifParser = new SIFFileParser(
+						file.getAbsolutePath());
+				sifParser.parseSIFFile();
+				this.taskStatus = Status.COMPLETED;
+			} catch (Exception e) {
+				this.taskStatus = Status.COMPLETED_WITH_ERRORS;
+				logger.error(e.getMessage());
+			}
+			break;
+		case ("xbel"):
+			try {
+				final XbelFileParser xbelParser = new XbelFileParser(
+						file.getAbsolutePath());
+				if (!xbelParser.getValidationState().isValid()) {
+					this.taskStatus = Status.COMPLETED_WITH_ERRORS;
+					throw new NdexException(
+							"XBEL file is has invalid elements.");
+				}
+				xbelParser.parseXbelFile();
+				this.taskStatus = Status.COMPLETED;
+			} catch (Exception e) {
+				this.taskStatus = Status.COMPLETED_WITH_ERRORS;
+				logger.error(e.getMessage());
+			}
+			break;
+		case ("xlsx"):
+		case ("xls"):
+			try {
+				final ExcelFileParser excelParser = new ExcelFileParser(
+						file.getAbsolutePath());
+				excelParser.parseExcelFile();
+				this.taskStatus = Status.COMPLETED;
+			} catch (Exception e) {
+				this.taskStatus = Status.COMPLETED_WITH_ERRORS;
+				logger.error(e.getMessage());
+			}
+		default:
+			file.delete();
+			logger.error("The uploaded file type is not supported; must be SIF, XBEL, or XLSX.");
+			this.taskStatus = Status.COMPLETED_WITH_ERRORS;
+
+		}
+		this.updateTaskStatus(this.taskStatus);
+	}
 
 }
